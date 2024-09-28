94ae1ef5f0372d5dd53d2a7c139c908ed337debd
Zack Novosad <zachary.novosad@gmail.com>
2024-09-28 10:31:13 -0500

    add layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,23 +1,19 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import type { PropsWithChildren } from "react";
+
+import { Layout } from "@/components/layout";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 export const Route = createRootRoute({
   component: () => (
     <Providers>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-      <hr />
-      <Outlet />
+      <Layout>
+        <Outlet />
+      </Layout>
     </Providers>
   ),
 });
@@ -26,9 +22,11 @@ const queryClient = new QueryClient();
 const Providers = ({ children }: PropsWithChildren) => {
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
-      <TanStackRouterDevtools />
-      <ReactQueryDevtools buttonPosition="bottom-right" />
+      <TooltipProvider>
+        {children}
+        <TanStackRouterDevtools />
+        <ReactQueryDevtools buttonPosition="bottom-right" />
+      </TooltipProvider>
     </QueryClientProvider>
   );
 };
